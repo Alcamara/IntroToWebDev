02be2b27d480579615b362cfa7fa3fcc4312c288
Sein <Alhousseinec@gmail.com>
2022-01-06 23:15:21 -0600

    code practice array solved 1&2

--- javascript/practice/05-arrays/your-code.js
+++ javascript/practice/05-arrays/your-code.js
@@ -10,19 +10,56 @@
 // Write your code here:
 
 
+
+function findMax(numbers) {
+  let max = 0;
+  let lenOfArray = numbers.length;
+  console.log(lenOfArray);
+
+  for(let position = 0; position < lenOfArray-1; position++){
+
+      if(lenOfArray === 1){
+        max = numbers[position];
+        break;
+      }
+      //if both value are the same skip iteration
+    if(numbers[position] === numbers[position+1]){
+      if(numbers[position] === max){
+        continue;
+      } else if (numbers[position] > max) {
+        max = numbers[position];
+      }
+    }
+
+    if (numbers[position] > numbers[position+1]) {
+      if (numbers[position] > max) {
+        max = numbers[position];
+      }
+    } else if (numbers[position] < numbers[position+1]) {
+      if (numbers[position+1] > max) {
+        max = numbers[position+1];
+      }
+    }
+  }
+
+  return max;
+}
+
+
+
 // Open index.html in your browser, open the JavaScript console, confirm the result.
 
 // Uncomment the code below to verify your function is working.
-// console.log("findMax =====");
-// var numbers = [46, 3, 40, 74, 63];
-// var actual = findMax(numbers);
-// console.log("[46, 3, 40, 74, 63] -> expected: 74, actual: %s", actual);
-// numbers = [3, 3, 3, 3];
-// actual = findMax(numbers);
-// console.log("[3, 3, 3, 3] -> expected: 3, actual: %s", actual);
-// numbers = [-1];
-// actual = findMax(numbers);
-// console.log("[-1] -> expected: -1, actual: %s", actual);
+console.log("findMax =====");
+var numbers = [46, 3, 40, 74, 63];
+var actual = findMax(numbers);
+console.log("[46, 3, 40, 74, 63] -> expected: 74, actual: %s", actual);
+ numbers = [3, 3, 3, 3];
+actual = findMax(numbers);
+console.log("[3, 3, 3, 3] -> expected: 3, actual: %s", actual);
+numbers = [-1];
+actual = findMax(numbers);
+console.log("[-1] -> expected: -1, actual: %s", actual);
 
 
 // 2. COMBINE ARRAYS
@@ -33,15 +70,37 @@
 // Write your code here:
 
 
+function combine(array1, array2) {
+  array1Len = array1.length;
+  array2Len = array2.length;
+  let newArray = [];
+
+   for(let position = 0; position < array1Len; position++){
+     newArray.push(array1[position]);
+   }
+
+   for(let position = 0; position < array2Len; position++){
+     newArray.push(array2[position]);
+   }
+
+   return newArray.toString();
+
+}
+
+var test = combine(["cat","dog","mouse","Ninja"],["lion","tiger","bear"])
+
+
+console.log(test);
+
 
 // Uncomment the code below to verify your function is working.
-// console.log("combine =====");
-// var merged = combine(["Oak", "Maple", "Birch"], ["Hydrogen", "Helium"]);
-// console.log("expected: Oak,Maple,Birch,Hydrogen,Helium\nactual: %s", merged);
-// merged = combine([12.5, 3, 855], [true, false, true]);
-// console.log("expected: 12.5,3,855,true,false,true\nactual: %s", merged);
-// merged = combine(["Saturn"], ["Black Lab", "Borzoi", "Pug"]);
-// console.log("expected: Saturn,Black Lab,Borzoi,Pug\nactual: %s", merged);
+console.log("combine =====");
+var merged = combine(["Oak", "Maple", "Birch"], ["Hydrogen", "Helium"]);
+console.log("expected: Oak,Maple,Birch,Hydrogen,Helium\nactual: %s", merged);
+merged = combine([12.5, 3, 855], [true, false, true]);
+console.log("expected: 12.5,3,855,true,false,true\nactual: %s", merged);
+merged = combine(["Saturn"], ["Black Lab", "Borzoi", "Pug"]);
+console.log("expected: Saturn,Black Lab,Borzoi,Pug\nactual: %s", merged);
 
 
 // 3. CHALLENGE: SUBTRACT AN ARRAY
